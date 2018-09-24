7fab04b152bb9c2a9e27b49524abec8c41cf1285
amigajac <43555963+amigajac@users.noreply.github.com>
2018-09-24 19:13:45 -0300

    Create form.php

---
+++ form.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<title>Atividade forum 4</title>
+<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+<h2>Formulario Atividade forum 4</h2>
+<form action="/action_page.php">
+<div class="form-group">
+<label for="nome">Nome:</label>
+<input type="nome" class="form-control" id="nome" placeholder="Nome" name="email">
+</div>
+<div class="form-group">
+<label for="email">Email:</label>
+<input type="email" class="form-control" id="email" placeholder="E-mail" name="email">
+</div>
+<div class="form-group">
+<label for="cemail">Confirma email:</label>
+<input type="cemail" class="form-control" id="cemail" placeholder="Confirma e-mail" name="cemail">
+</div>
+<div class="form-group">
+<label for="pwd">Senha:</label>
+<input type="password" class="form-control" id="pwd" placeholder="Senha" name="pwd">
+</div>
+<div class="form-group">
+<label for="pwd">Confirma senha:</label>
+<input type="cpassword" class="form-control" id="cpwd" placeholder="Confirma senha" name="cpwd">
+</div>
+
+<button type="submit" class="btn btn-default">Enviar</button>
+</form>
+</div>
+
+</body>
+</html>
