08f486137699f9ce8fe36ec6f322afd6fc8ff3eb
amigajac <43555963+amigajac@users.noreply.github.com>
2018-09-24 21:25:06 -0300

    Update form.php

--- form.php
+++ form.php
@@ -12,27 +12,66 @@
 <body>
 
 <div class="container">
-<h2>Formulario Atividade forum 4</h2>
+<h2>Cadastro Morador</h2>
 <form action="/action_page.php">
 <div class="form-group">
-<label for="nome">Nome:</label>
-<input type="nome" class="form-control" id="nome" placeholder="Nome" name="email">
+<label for="nome">Nome completo:</label>
+<input type="nome" class="form-control" id="nome" placeholder="Nome" name="nome">
 </div>
+<div class="form-group">
+<label for="cpf">CPF:</label>
+<input type="cpf" class="form-control" id="cpf" placeholder="CPF" name="cpf">
+</div>
+
+<div class="form-group">
+<label for="rg">RG:</label>
+<input type="rg" class="form-control" id="rg" placeholder="RG" name="rg">
+</div>
+
+<div class="form-group">
+<label for="endereco">Endereço:</label>
+<input type="endereco" class="form-control" id="endereco" placeholder="Endereço" name="endereco">
+</div>
+
+<div class="form-group">
+<label for="numero">Número:</label>
+<input type="numero" class="form-control" id="numero" placeholder="Número" name="numero">
+</div>
+
+<div class="form-group">
+<label for="cep">CEP:</label>
+<input type="cep" class="form-control" id="cep" placeholder="CEP" name="cep">
+</div>
+
+
+<div class="form-group">
+<label for="bairro">Bairro:</label>
+<input type="bairro" class="form-control" id="bairro" placeholder="Bairro" name="bairro">
+</div>
+
+<div class="form-group">
+<label for="cidade">Cidade:</label>
+<input type="cidade" class="form-control" id="cidade" placeholder="Cidade" name="cidade">
+</div>
+
+<div class="form-group">
+<label for="estado">Estado:</label>
+<input type="estado" class="form-control" id="estado" placeholder="Estado" name="estado">
+</div>
+
+<div class="form-group">
+<label for="celular">Celular:</label>
+<input type="Celular" class="form-control" id="celular" placeholder="Celular" name="celular">
+</div>
+
 <div class="form-group">
 <label for="email">Email:</label>
 <input type="email" class="form-control" id="email" placeholder="E-mail" name="email">
 </div>
-<div class="form-group">
-<label for="cemail">Confirma email:</label>
-<input type="cemail" class="form-control" id="cemail" placeholder="Confirma e-mail" name="cemail">
-</div>
+
 <div class="form-group">
 <label for="pwd">Senha:</label>
 <input type="password" class="form-control" id="pwd" placeholder="Senha" name="pwd">
-</div>
-<div class="form-group">
-<label for="pwd">Confirma senha:</label>
-<input type="cpassword" class="form-control" id="cpwd" placeholder="Confirma senha" name="cpwd">
 </div>
 
 <button type="submit" class="btn btn-default">Enviar</button>
